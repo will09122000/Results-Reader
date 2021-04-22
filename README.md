73d08d626018926d1ceca4a40bd09f4267ef6c6f
William Coates <will0912@icloud.com>
2021-04-22 12:53:12 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Results-Reader
- 
+
+How to run:
+
+1. Install Python 3
+   https://www.python.org/downloads/
+2. `pip install opencv-python`
+3. `pip install pytesseract`
+3. `pip install pandas`
+4. Download Tesseract and install to 'Program Files'
+   https://digi.bib.uni-mannheim.de/tesseract/tesseract-ocr-w64-setup-v5.0.0-alpha.20201127.exe
+5. Make sure results screenshots are in the results folder named 1 to 5
+6. To run: 'python3 reader.py'
---
+++ reader.py
@@ -0,0 +1,97 @@
+import cv2
+import pytesseract
+import numpy as np
+import pandas
+
+def main():
+    for stage_num in range(1, 6):
+        words, num_entries = read_image(stage_num)
+
+        drivers, vehicles, times = format_data(words, num_entries)
+
+        # Write to CSV
+        df = pandas.DataFrame(data={
+                                    "driver": drivers,
+                                    "vehicle": vehicles,
+                                    "stage": times
+                                    })
+        file_path = "./results/{}.csv".format(stage_num)
+        df.to_csv(file_path, sep=',', index=False)
+
+        print("Successfully created CSV file for stage " + str(stage_num))
+
+def read_image(stage_num):
+    # Get stage screenshot
+    img_path = r"C:\Users\will0\Documents\GitHub\Results-Reader\stages\{}.png".format(stage_num)
+    img = cv2.imread(img_path)
+
+    # Convert to black and white
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    gray, img_bin = cv2.threshold(gray,128,255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    gray = cv2.bitwise_not(img_bin)
+
+    # Read text from image
+    kernel = np.ones((2, 1), np.uint8)
+    img = cv2.erode(gray, kernel, iterations=1)
+    img = cv2.dilate(img, kernel, iterations=1)
+    pytesseract.pytesseract.tesseract_cmd = (r'C:\Program Files\Tesseract-OCR\tesseract.exe')
+    out_below = pytesseract.image_to_string(img)
+
+    # Remove blanks
+    words = list(filter(None, out_below.split('\n')))
+
+    # Get number of entries
+    num_entries = 0
+    for word in words:
+        if ":" in word and "." in word:
+            num_entries += 1
+
+    return words, num_entries
+
+def format_data(words, num_entries):
+
+    # Split list
+    drivers = words[1:num_entries+1]
+    vehicles = words[num_entries+2:num_entries*2+2]
+    times = words[num_entries*2+3:num_entries*3+3]
+
+    # Fix driver names
+    for i in range(len(drivers)):
+        if "ELETAL" in drivers[i] or "A | " in drivers[i]:
+            drivers[i] = "Mr. Beletal"
+        elif "WHLLO9I2" in drivers[i] or "OPT2" in drivers[i] or "OT2" in drivers[i] or "912" in drivers[i] or "WILLO" in drivers[i]:
+            drivers[i] = "Will0912"
+        elif "LIGHT" in drivers[i]:
+            drivers[i] = "L1GHTN1NG14"
+        elif "XSEMP" in drivers[i]:
+            drivers[i] = "XSempiternal012"
+        elif "VMUSHROOM" in drivers[i]:
+            drivers[i] = "V.Mushroom"
+        elif "CALL" in drivers[i]:
+            drivers[i] = "Calli"
+        elif "MIN" in drivers[i]:
+            drivers[i] = "Suk Min Dik"
+        elif "Rope" in drivers[i] or "DROP" in drivers[i]:
+            drivers[i] = "Dropi"
+        else:
+            drivers[i] = drivers[i].title()
+
+    # Fix vehicle names
+    for i in range(len(vehicles)):
+        vehicles[i] = vehicles[i].title()
+        if "$" in vehicles[i]:
+            vehicles[i] = vehicles[i].replace("$", "S")
+        elif "Citroen" in vehicles[i]:
+            vehicles[i] = vehicles[i].replace("Citroen", "Citroën")
+        elif "Rs" in vehicles[i]:
+            vehicles[i] = vehicles[i].replace("Rs", "RS")
+
+    # Fix stage times
+    for i in range(len(times)):
+        if (len(times[i]) != 9):
+            times[i] = times[i][0:-1]
+    
+    return drivers, vehicles, times
+
+if __name__ == "__main__":
+    main()
